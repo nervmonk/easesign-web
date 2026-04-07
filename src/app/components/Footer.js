2a93749f326f8305ae0648e7f4fea753321c4ac8
dwikyryan <dwikyryan@gmail.com>
2026-04-07 23:32:41 +0700

    modify icon

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -40,19 +40,14 @@ const InstagramIcon = () => (
     />
   </svg>
 );
-const RssIcon = () => (
+const LinkedInIcon = () => (
   <svg
     className="h-6 w-6"
     fill="currentColor"
     viewBox="0 0 24 24"
     aria-hidden="true"
   >
-    <path d="M4.5 3a1.5 1.5 0 011.5 1.5v1.5a1.5 1.5 0 01-3 0V4.5A1.5 1.5 0 014.5 3z" />
-    <path
-      fillRule="evenodd"
-      d="M19.5 3A1.5 1.5 0 0121 4.5v15a1.5 1.5 0 01-1.5 1.5h-15A1.5 1.5 0 013 19.5V11.25a1.5 1.5 0 013 0v7.5a.75.75 0 00.75.75h13.5a.75.75 0 00.75-.75V5.25a.75.75 0 00-.75-.75H12.75a1.5 1.5 0 010-3h6.75zM8.25 7.5a1.5 1.5 0 011.5-1.5h1.5a1.5 1.5 0 010 3H9.75a1.5 1.5 0 01-1.5-1.5zM1.5 4.5a3 3 0 013-3h.008a3 3 0 013 3v.008a3 3 0 01-3 3H4.5a3 3 0 01-3-3V4.5z"
-      clipRule="evenodd"
-    />
+    <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
   </svg>
 );
 
@@ -153,7 +148,7 @@ const Footer = ({ lang = 'id', dict = {} }) => {
             &copy; {new Date().getFullYear()} PT Paramita Digital Nusantara. All rights reserved.
           </p>
           <div className="flex items-center space-x-6">
-            <Link href="#" className="text-gray-500 hover:text-cyan-400 transition-colors transform hover:-translate-y-1 duration-300">
+            {/* <Link href="#" className="text-gray-500 hover:text-cyan-400 transition-colors transform hover:-translate-y-1 duration-300">
               <FacebookIcon />
             </Link>
             <Link href="#" className="text-gray-500 hover:text-cyan-400 transition-colors transform hover:-translate-y-1 duration-300">
@@ -161,9 +156,9 @@ const Footer = ({ lang = 'id', dict = {} }) => {
             </Link>
             <Link href="#" className="text-gray-500 hover:text-cyan-400 transition-colors transform hover:-translate-y-1 duration-300">
               <InstagramIcon />
-            </Link>
-            <Link href="#" className="text-gray-500 hover:text-cyan-400 transition-colors transform hover:-translate-y-1 duration-300">
-              <RssIcon />
+            </Link> */}
+            <Link href="https://www.linkedin.com/company/easesign-id/" target="_blank" className="text-gray-500 hover:text-cyan-400 transition-colors transform hover:-translate-y-1 duration-300">
+              <LinkedInIcon />
             </Link>
           </div>
         </div>
